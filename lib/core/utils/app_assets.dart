19036b59665e6c7be652c2b89bda12fdf18c0761
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-07-14 07:22:09 +0300

    folders and assets created

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,32 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesForgetpassword
+  /// assets/images/forgetpassword.png
+  static const String imagesForgetpassword = "assets/images/forgetpassword.png";
+
+  /// Assets for imagesOnboarding1
+  /// assets/images/onboarding1.png
+  static const String imagesOnboarding1 = "assets/images/onboarding1.png";
+
+  /// Assets for imagesOnboarding2
+  /// assets/images/onboarding2.png
+  static const String imagesOnboarding2 = "assets/images/onboarding2.png";
+
+  /// Assets for imagesOnboarding3
+  /// assets/images/onboarding3.png
+  static const String imagesOnboarding3 = "assets/images/onboarding3.png";
+
+  /// Assets for imagesSuccess
+  /// assets/images/Success.png
+  static const String imagesSuccess = "assets/images/Success.png";
+
+  /// Assets for imagesVector
+  /// assets/images/Vector.svg
+  static const String imagesVector = "assets/images/Vector.svg";
+
+  /// Assets for imagesVector2
+  /// assets/images/Vector2.svg
+  static const String imagesVector2 = "assets/images/Vector2.svg";
+}
+
