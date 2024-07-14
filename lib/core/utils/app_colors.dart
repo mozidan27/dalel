3729b9977527ce9880e213c6fdaea4beca057149
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-07-14 07:28:11 +0300

    APP COLORS

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color primaryColor = const Color(0xFFC49385);
+  static Color offWhite = const Color(0xFFF8F4F9);
+  static Color deebBrown = const Color(0xFF6B4B3E);
+  static Color grey = const Color(0xFFC4C4C4);
+  static Color deebGrey = const Color(0xFF6F6460);
+  static Color lightGrey = const Color(0xFFB4B4B4);
+  static Color black = const Color(0xFF333333);
+}
