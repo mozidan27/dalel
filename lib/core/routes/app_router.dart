bc341dbd77947efe1124c771e5db21ab2a62a4e5
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-07-21 07:50:47 +0300

    firebase linked to our project

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,14 +1,23 @@
-import 'package:dalel/features/auth/presentation/views/sign_in.dart';
-import 'package:dalel/features/auth/presentation/views/sign_up.dart';
+import 'package:dalel/core/services/service_locator.dart';
+import 'package:dalel/features/auth/presentation/auth_cubit/cubit/auth_cubit.dart';
+import 'package:dalel/features/auth/presentation/views/sign_in_view.dart';
+import 'package:dalel/features/auth/presentation/views/sign_up_view.dart';
 import 'package:dalel/features/home/presentation/views/onboarding_view.dart';
 import 'package:dalel/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Map<String, WidgetBuilder> buildAppRoutes() {
   return {
     '/onboardingview': (context) => const OnboardingView(),
     '/splashview': (context) => const SplashView(),
-    '/signup': (context) => const SignUpView(),
-    '/signin': (context) => const SignInView(),
+    '/signup': (context) => BlocProvider(
+          create: (context) => getIt<AuthCubit>(),
+          child: const SignUpView(),
+        ),
+    '/signin': (context) => BlocProvider(
+          create: (context) => getIt<AuthCubit>(),
+          child: const SignInView(),
+        ),
   };
 }
